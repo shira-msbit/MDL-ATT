b645492d064d89523c6d48a91de367c77a29fc57
shira-msbit <shira@msbitsoftware.com>
2017-08-02 19:58:01 +0300

    creating buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {MdlModule} from '@angular-mdl/core';
+import { AttButtonComponent } from './att-button/att-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AttButtonComponent
   ],
   imports: [
     BrowserModule,
