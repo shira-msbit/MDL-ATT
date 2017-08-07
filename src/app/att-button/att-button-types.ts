717bb82fa71fb97875f7ee6517621f669671a96e
shira-msbit <shira@msbitsoftware.com>
2017-08-07 16:18:43 +0300

    creating button component without att stylesheet

---
+++ src/app/att-button/att-button-types.ts
@@ -0,0 +1,4 @@
+// holds the att button types
+
+type roleType = 'primary-functional' | 'primary-promo' | 'primary-special' | 'primary-secondary';
+type iconType = 'add-maximize';
--- src/app/att-button/att-button.component.ts
+++ src/app/att-button/att-button.component.ts
@@ -2,14 +2,14 @@ import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'att-button',
-  template: `<mdl-button class="mdl-button role-{{role}} icon-{{icon}}" [disabled]="disable">
-    <mdl-icon class="icon-add-maximize"></mdl-icon>click here</mdl-button>`,
+  template: `<mdl-button class="mdl-button btn-{{role}} width-role-{{widthRole}}" [disabled]="disable">
+    <mdl-icon class="icon-{{icon}}"></mdl-icon>click here</mdl-button>`,
   styleUrls: ['./att-button.component.scss']
 })
 export class AttButtonComponent implements OnInit {
-// @Input() width: string;
+  @Input() widthRole: string;
   @Input() role: string;
-  @Input() icon: string;
+  @Input() icon: string ;
   @Input() disable: boolean;
 
   constructor() {
